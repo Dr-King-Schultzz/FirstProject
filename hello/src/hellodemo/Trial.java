4346701e271743f535a430f9799e669850950510
Dr-King-Schultzz <89225942+Dr-King-Schultzz@users.noreply.github.com>
2021-10-23 23:26:05 +0530

    Update Trial.java
    updated code AT GIT

--- hello/src/hellodemo/Trial.java
+++ hello/src/hellodemo/Trial.java
@@ -3,6 +3,9 @@ package hellodemo;
 public class Trial {
 
 	public static void main(String[] args) {
+		//calculating mean
+		// code changed at git
+		
 		int a=159, b=574, c=799;
 		float m=(a+b+c)/3;
 		System.out.println("Mean is "+m);
