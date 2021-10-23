78fa4c630a6261b66114823087c518a66b662863
Dr-King-Schultzz <89225942+Dr-King-Schultzz@users.noreply.github.com>
2021-10-23 23:27:10 +0530

    Update Trial.java
    2ND UPDATE

--- hello/src/hellodemo/Trial.java
+++ hello/src/hellodemo/Trial.java
@@ -4,7 +4,7 @@ public class Trial {
 
 	public static void main(String[] args) {
 		//calculating mean
-		// code changed at git
+		System.out.println(" code changed at git");
 		
 		int a=159, b=574, c=799;
 		float m=(a+b+c)/3;
